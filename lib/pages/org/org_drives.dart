1f1f496163a6109073ca2eb053155c30f9668ccb
jabarro <jdabarro1@up.edu.ph>
2024-05-21 15:15:31 +0800

    feat: donation drive detail

--- lib/pages/org/org_drives.dart
+++ lib/pages/org/org_drives.dart
@@ -8,6 +8,13 @@ class OrganizationDrives extends StatefulWidget {
 }
 
 class _OrganizationDrivesState extends State<OrganizationDrives> {
+  String name = "McDonaldo";
+  String desc = "One of the fast foods of all time";
+  List<String> listOfDonations = [
+    "Cheeseburger",
+    "ChickenFillet"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +33,20 @@ class _OrganizationDrivesState extends State<OrganizationDrives> {
               },
               icon: Icon(Icons.more_horiz),
             ),
+            onTap: () {
+              showModalBottomSheet(context: context, builder: (_){
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Text(name, style: TextStyle(fontSize: 20),),
+                      Text(desc, style: TextStyle(fontSize: 20),),
+                      Text("Donations:", style: TextStyle(fontSize: 20),),
+                    ],
+                  ),
+                );
+              });
+            },
           );
         },
       ),
