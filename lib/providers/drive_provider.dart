79063934e2f392fee07b589bcd677ff078a8a50d
jabarro <jdabarro1@up.edu.ph>
2024-05-28 14:32:49 +0800

    feat: added provider for drives

---
+++ lib/providers/drive_provider.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/api/firebase_drive_api.dart';
+import 'package:my_app/models/donation.dart';
+import 'package:my_app/models/donation_drive.dart';
+
+class DriveProvider with ChangeNotifier {
+  late FirebaseDriveAPI driveService;
+  late Stream<QuerySnapshot> _driveStream;
+
+  DonationDrive? driveObj;
+
+  DriveProvider() {
+    driveService = FirebaseDriveAPI();
+    fetchDrives();
+  }
+
+  Stream<QuerySnapshot> get drives => _driveStream;
+  void fetchDrives(){
+    _driveStream = driveService.getAllDrives();
+    notifyListeners();
+  }
+
+  void addDrive(String name, String desc, List<Donation> donations) async {
+    String message = await driveService.addDrive(name, desc, donations);
+    print(message);
+    notifyListeners();
+  }
+}
